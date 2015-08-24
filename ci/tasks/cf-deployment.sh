ea7f4f893e8d8f2c54982f66ec5d62dc039aee8b
Shaozhen Ding <dsz0111@gmail.com>
2015-08-24 16:34:32 -0500

    add all the deployments

--- ci/tasks/cf-deployment.sh
+++ ci/tasks/cf-deployment.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-target="cf api $API_ENDPOINT --skip-ssl-verify"
+target="cf api $API_ENDPOINT --skip-ssl-validation"
 echo $target
+exec $target
+
+echo "Login....."
+login="cf auth $USERNAME $PASSWORD"
+echo $login
+exec $login
+
+echo "push the app"
+push="cf push -n $HOST -p music-rc/spring-music.*.war"
+echo $push
+exec $push
