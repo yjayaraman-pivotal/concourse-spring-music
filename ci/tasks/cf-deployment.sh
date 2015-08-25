69952f9416f0adc4a369f8febc68d244e62dac73
Shaozhen Ding <dsz0111@gmail.com>
2015-08-25 14:00:22 -0500

    rename the music-rc release

--- ci/tasks/cf-deployment.sh
+++ ci/tasks/cf-deployment.sh
@@ -15,6 +15,6 @@ org_space="cf target -o $ORG -s $SPACE"
 eval $org_space
 
 echo "push the app"
-push="cf push music -n $HOST -p music-rc/spring-music-*.war -m 512m"
+push="cf push music -n $HOST -p music-rc-release/spring-music-*.war -m 512m"
 #echo $push
 eval $push
