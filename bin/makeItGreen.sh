755b237ebb5498806e8f3984ba69c6cf068faaec
Brian Byers <bbyers@pivotal.io>
2016-07-29 10:39:54 -0500

    fixing paths

--- bin/makeItGreen.sh
+++ bin/makeItGreen.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-#cd ~/repos/concourse-spring-music
-cd ~/Projects/demos/concourse-spring-music
-cp src/main/webapp/assets/css/green-app.css src/main/webapp/assets/css/app.css
-git add src/main/webapp/assets/css/app.css
+cp ../src/main/webapp/assets/css/green-app.css ../src/main/webapp/assets/css/app.css
+git add ../src/main/webapp/assets/css/app.css
 git commit -m 'making banner green'
 git push
